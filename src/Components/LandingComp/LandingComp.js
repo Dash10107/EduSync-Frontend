8bb59578ff2a77c1614ebb21a94f8384878079d1
Dash10107 <dakshcjain@gmail.com>
2023-09-13 21:31:19 +0530

    Completed Login Functionality

--- src/Components/LandingComp/LandingComp.js
+++ src/Components/LandingComp/LandingComp.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { ToastContainer} from 'react-toastify';
 
 const LandingComp = () => {
   return (
     <div>
       LandingComp
+      <ToastContainer/>
     </div>
   )
 }
--- src/Components/LoginComp/LoginComp.js
+++ src/Components/LoginComp/LoginComp.js
@@ -1,17 +1,79 @@
 import React, { useState } from "react"
 import "./LoginComp.css"
-import { Button } from "@mui/material";
-
+// import { Button } from "@mui/material";
+// import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
 const LoginComp = (props) => {
 
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  const onSubmit = (e) => {
-    e.preventDefault(); // Prevents the default form submission behavior
-    // Add your login logic here using the userName and password state values
+//code to ping backend
+  //   useEffect(()=>{
+// try {
+//        axios.get('http://localhost:5000/').then(res => {
+//       console.log(res.data.message);
+//       })
+//     } catch (error) {
+//       console.log("error", error)
+//     }
+//   },[])
+
+  const onSubmit = async (e) => {
+    e.preventDefault(); 
     console.log("Submitted");
+ const phrase = "somaiya.edu";
+
+if (!userName.endsWith(phrase)) {
+  console.log("The string does not end with 'somaiya.edu'");
+  toast.warning("Please use somaiya.edu");
+    setUserName("");
+    setPassword("");
+  return;
+}
+
+    let loginInfo={
+      email:userName,
+      password:password
+    }
+       console.log(loginInfo);
+try{
+      const response = await fetch('http://localhost:5000/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(loginInfo)
+
+        });
+
+        const result = await response.json();
+        console.warn("Result", result);
+
+        if(result.success){
+        if(result.token){
+          localStorage.setItem("token",result.token);
+          setUserName("");
+          setPassword("");
+          toast.success("Successfully Logged In")
+          navigate("/");
+        }else{
+          console.log(result.token);
+        }
+        }else{
+          console.log(result);
+          
+        }
+
+      } catch (error) {
+        console.error(error);
+        toast.error(error);
+      }
+
   }
+
 
   return (
     <div className="login-main-div">
@@ -23,11 +85,11 @@ const LoginComp = (props) => {
           <form onSubmit={onSubmit} className="login-card">
             <div className="input-group">
               <label htmlFor="email" className="login-label">Email ID</label>
-              <input type="text" id="email" className="login-input" name="email" placeholder="Enter your email"></input>
+              <input type="email" id="email" className="login-input" name="email" onChange={(e)=>{setUserName(e.target.value)}} value={userName} required={true}></input>
             </div>
             <div className="input-group">
               <label htmlFor="password" className="login-label">Password</label>
-              <input type="password" id="password" className="login-input" name="password" placeholder="Enter your password"></input>
+              <input type="password" id="password" className="login-input" name="password" onChange={(e)=>{setPassword(e.target.value)}} required={true} value={password}></input>
             </div>
             <div className="login-or-comp">
               <p>Or</p>
@@ -40,6 +102,7 @@ const LoginComp = (props) => {
             </div>
           </form>
         </div>
+        <ToastContainer/>
       </div>
     
   )
