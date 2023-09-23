fc06e92892734d4456b6eb7c005ab9808d0d74cd
Dash10107 <dakshcjain@gmail.com>
2023-09-23 22:10:11 +0530

    cleared till questions

--- src/Components/SubjectComponents/SubjectComp.js
+++ src/Components/SubjectComponents/SubjectComp.js
@@ -1,15 +1,64 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import  "./SubjectComp.css"
 import Navbar from "../../Layouts/Navbar/Navbar";
 import axios from "axios";
+import SingleTopic from "./singleTopic/SingleTopic";
 
 const SubjectComp = (props) => {
-    console.log("ModuleId",localStorage.getItem("moduleId"));
-    console.log("ChapterId",localStorage.getItem("chapterId"));    
+  const moduleId = localStorage.getItem("moduleId");
+  const chapterId = parseInt(localStorage.getItem("chapterId"));
+
+    const [chapters,setChapters] = useState([]);
+    const [subchapters,setSubChapters] = useState([]);
+      const [loading,setLoading] = useState(true);
+    
+      const fetchChapters=async()=>{
+        try {
+          await axios.get(`http://localhost:5000/module/chapters/${moduleId}`,{
+            headers: {
+              Authorization: localStorage.getItem("token"),
+            }
+           }).then(response => {
+            console.log("Response",response);
+            
+            if(response.status===200){
+            setChapters(response.data.chapters);
+            const chapter = response.data.chapters.find((chap) => chap.id === chapterId);            
+            setSubChapters(chapter ? chapter.subtopics : []);
+    
+            }else{console.log("Status Code",response.status);
+            }
+           
+          })
+        } catch (error) {
+          console.log("error", error)
+        }finally {
+          // Set loading to false when data fetching is complete
+          setLoading(false);
+        }
+      }
+    
+    useEffect(()=>{
+    fetchChapters();
+    },[])
+    
+    useEffect(()=>{
+      console.log(chapters);
+      console.log(subchapters);
+      
+    },[chapters,subchapters])
+    
+    
   return (
     <div>
     <Navbar/>
-      Subject Comp
+      <h2>Subtopics</h2><br/>
+      <ul>
+        {subchapters?.map((subtopic) => (
+         <SingleTopic subtopic={subtopic} />
+        ))}
+        
+      </ul>
     </div>
   )
 };
---
+++ src/Components/SubjectComponents/singleTopic/SingleTopic.js
@@ -0,0 +1,19 @@
+import React from "react"
+import  "./SingleTopic.css"
+import { useNavigate } from "react-router-dom";
+
+const SingleTopic = (props) => {
+    const {subtopic} = props;
+    const navigate = useNavigate();
+      return (
+    <div onClick={()=>{
+      localStorage.setItem("subChapterId",subtopic.id);
+      navigate("/question");
+    }}>
+        <li key={subtopic.id}>{subtopic.name}<br/><br/></li>
+  <br/>
+  </div>
+  )
+};
+
+export default SingleTopic;
