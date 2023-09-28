d9cca151d11fe0ab35009c284769c13be0adb210
Dash10107 <dakshcjain@gmail.com>
2023-09-28 10:33:29 +0530

    made post progress

--- src/Components/QuestionsComp/QuestionComp.js
+++ src/Components/QuestionsComp/QuestionComp.js
@@ -3,11 +3,12 @@ import "./QuestionComp.css";
 import axios from "axios";
 import Navbar from "../../Layouts/Navbar/Navbar";
 import SingleQuestion from "./singleQuestion/SingleQuestion";
-
+import { useNavigate } from "react-router-dom";
 const QuestionComp = (props) => {
   const moduleId = localStorage.getItem("moduleId");
   const chapterId = parseInt(localStorage.getItem("chapterId"));
   const subChapterId = localStorage.getItem("subChapterId");
+  const navigate = useNavigate();
 
   const [questions, setQuestions] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -15,6 +16,40 @@ const QuestionComp = (props) => {
   const [selectedOption, setSelectedOption] = useState("");
   const [showResult, setShowResult] = useState(false); // To control when to show the result button
   const [correctAnswersCount, setCorrectAnswersCount] = useState(0); // To count correct answers
+
+
+  
+
+  const handleEndTest = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      moduleId: moduleId,
+      chapterId: chapterId,
+      subchapterId: subChapterId,
+      correctAnswers:correctAnswersCount ,
+      totalQuestions:questions.length,
+    }
+
+    try {
+      const response = await axios.post("http://localhost:5000/progress/", formData, {
+        headers: {
+          Authorization: localStorage.getItem("token"), // Include your authorization token here
+        },
+      });
+
+      if (response.status === 200) {
+        console.log("Progress saved successfully");
+        console.log('Response',response);
+        setCorrectAnswersCount(0);
+        navigate(-1); 
+      } else {
+        console.error("Failed to save progress");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
 
   const fetchQuestions = async () => {
@@ -45,18 +80,31 @@ const QuestionComp = (props) => {
   };
 
   const handleShowResult = () => {
+    if(!selectedOption) {
+      // Display an error or message to select an option
+      console.log("Please select an option before proceeding.");
+      return;
+    }else{
     setShowResult(true);
+    }
   };
 
-  const handleEndTest = () => {
-    console.log("Test Ended");
-  };
+  
 
 
 
   const handleNextQuestion = () => {
-    if (selectedOption) {
-      // If an option is selected, check if it's the correct answer
+
+    if(!selectedOption) {
+      // Display an error or message to select an option
+      console.log("Please select an option before proceeding.");
+      return;
+    }
+    else {
+   
+
+      if (currentQuestionIndex < questions.length - 1) {
+           // If an option is selected, check if it's the correct answer
       const currentQuestion = questions[currentQuestionIndex];
       if (currentQuestion.correctAnswer === selectedOption) {
         // Handle when the answer is correct
@@ -68,8 +116,6 @@ const QuestionComp = (props) => {
         // You can perform any other actions here
         console.log("Incorrect.");
       }
-
-      if (currentQuestionIndex < questions.length - 1) {
         // If there are more questions, increment the index to move to the next question
         setCurrentQuestionIndex(currentQuestionIndex + 1);
         setSelectedOption(""); // Clear the selected option for the next question
@@ -79,11 +125,11 @@ const QuestionComp = (props) => {
         console.log("Test Completed");
         console.log(`You got ${correctAnswersCount} questions correct out of ${questions.length}`);
       }
-    } else {
-      // Display an error or message to select an option
-      console.log("Please select an option before proceeding.");
+      
     }
   };
+
+
 
   useEffect(() => {
     fetchQuestions();
