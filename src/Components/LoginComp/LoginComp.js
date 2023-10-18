ad3050bdef7eb2cd2951b5021470b3bcecda0dbc
Dash10107 <dakshcjain@gmail.com>
2023-10-18 22:22:48 +0530

    took moksh changes

--- src/Components/LoginComp/LoginComp.js
+++ src/Components/LoginComp/LoginComp.js
@@ -64,15 +64,10 @@ const LoginComp = (props) => {
 
       if (result.success) {
         if (result.token) {
-           // Check if it's the user's first login
-           if (!localStorage.getItem("firstLogin")) {
-            localStorage.setItem("firstLogin", "true");
-          }
           localStorage.setItem("token", result.token);
           setUserName("");
           setPassword("");
-          // Redirect to the home page or handle it in your route configuration
-         
+          navigate("/home");
         } else {
           console.log(result.token);
         }
@@ -96,12 +91,12 @@ const LoginComp = (props) => {
       <Navbar />
 
       {/* daksh ye div mene add kiya hai image and login ko ek container mein daalne k liye  */}
-      <div className="flex md:space-x-14 lg:space-x-14 bg-[#f3f2f7] h-full justify-center">
+      <div className="flex md:space-x-14 lg:space-x-14  h-full justify-center">
 
         {/* ye img hai login card k baju wala */}
 
         <div>
-          <img src="https://media.istockphoto.com/id/1452604857/photo/businessman-touching-the-brain-working-of-artificial-intelligence-automation-predictive.webp?b=1&s=170667a&w=0&k=20&c=iJp6e2C-l2lRmyG3ColHMpXe0QYrPnrfQQc2O6PsYC4=" alt="help" className="h-[28rem] hidden rounded md:block
+          <img src="https://www.alphalearn.com/wp-content/uploads/2021/11/e-learning-2.jpg" alt="help" className="h-[28rem] hidden rounded md:block
           // responsive design css
           md:w-[30rem] lg:w-[48rem] ml-8 mt-10 2xl:w-[55rem] 2xl:h-[34rem]" />
         </div>
@@ -111,13 +106,21 @@ const LoginComp = (props) => {
 hover:ring-4 ring-blue-950 ring-offset-4 */}
 
         {/* This is login card */}
-        <div className="md:mt-[6.5rem] mb-[200px] rounded-[20px] 
+        <div className="md:mt-[6.5rem] mb-[200px] rounded-[20px]
         // responsive css design 
       md:w-[28rem] md:ml-0 md:mr-24 md:h-96
       lg:w-[33rem] lg:ml-0 ">
 
-          <form onSubmit={onSubmit} className="login-card space-y-10 mt-20 md:mt-0 sm:mr-5 md:h-80 md:space-y-5 
+
+
+          <form onSubmit={onSubmit} className="login-card space-y-14 pt-6 mt-20 md:mt-0 sm:mr-5 md:h-80 md:space-y-5 
           2xl:h-[26rem] 2xl:space-y-10">
+
+            <div className="input-group text-2xl font-bold">
+            {/* yape font awesome ka icon add krna hai */}
+              LOGIN
+            </div>
+
             <div className="input-group">
               <input
                 type="email"
@@ -129,6 +132,7 @@ hover:ring-4 ring-blue-950 ring-offset-4 */}
                 required={true}
               ></input>
             </div>
+
             <div className="input-group" style={{ display: "flex", alignItems: "center" }}>
               <input
                 type={passwordShown ? "text" : "password"}
@@ -144,7 +148,8 @@ hover:ring-4 ring-blue-950 ring-offset-4 */}
                 {eye}
               </i>
             </div>
-{/* sm:ml-[0rem] md:ml-[0rem] lg:ml-[3.5rem] */}
+
+            {/* sm:ml-[0rem] md:ml-[0rem] lg:ml-[3.5rem] */}
             <div className=" space-y-1 self-center ">
               <div className="flex ">
                 <hr className="bg-black text-black w-[7rem] lg:w-[10rem] mt-3 mr-3" />
@@ -154,7 +159,7 @@ hover:ring-4 ring-blue-950 ring-offset-4 */}
 
               <div>Not an Account? <span onClick={() => { navigate("/signup") }}>Signup</span></div>
             </div>
-{/* sm:ml-[0rem] md:ml-[4rem] lg:ml-[10rem] */}
+            {/* sm:ml-[0rem] md:ml-[4rem] lg:ml-[10rem] */}
             <div className=" bg-[#CCC9DC] border-2 border-blue-950 text-black px-10  font-bold py-0.5 rounded-lg text-xl font-serif hover:ring-2 ring-[#CCC9DC] self-center">
               <button type="submit" className="login-submit-btn">
                 Login
--- src/Components/SignupComp/SignupComp.js
+++ src/Components/SignupComp/SignupComp.js
@@ -19,19 +19,19 @@ const SignupComp = (props) => {
   const { name, email, password, password2 } = formData;
 
   const navigate = useNavigate();
-const [errors,setErrors] = useState({});
-  const [toastOpen,setToastOpen] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [toastOpen, setToastOpen] = useState(false);
   const [passwordShown, setPasswordShown] = useState(false);
   const togglePasswordVisiblity = () => {
     setPasswordShown(passwordShown ? false : true);
   };
-  const eye = passwordShown?<FontAwesomeIcon icon={faEye} />:<FontAwesomeIcon icon={faEyeSlash} />
+  const eye = passwordShown ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />
 
   const [passwordShown2, setPasswordShown2] = useState(false);
   const togglePasswordVisiblity2 = () => {
     setPasswordShown2(passwordShown2 ? false : true);
   };
-  const eye2 = passwordShown2?<FontAwesomeIcon icon={faEye} />:<FontAwesomeIcon icon={faEyeSlash} />
+  const eye2 = passwordShown2 ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />
   // Function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -40,7 +40,7 @@ const [errors,setErrors] = useState({});
 
     if (!email.endsWith(phrase)) {
       console.log("The email does not end with 'somaiya.edu",);
-      setErrors({error:"Please use somaiya.edu Email",email});
+      setErrors({ error: "Please use somaiya.edu Email", email });
       setToastOpen(true);
       return;
     }
@@ -50,27 +50,27 @@ const [errors,setErrors] = useState({});
         "http://localhost:5000/users/register",
         formData
       );
-      console.log("Response",response)
-      if(response.status===200){
-           // If the registration is successful, you can handle the response here
-      
-      Login(email,password);
-     setFormData({
-    name: "",
-    email: "",
-    password: "",
-    password2: "",
-  });
+      console.log("Response", response)
+      if (response.status === 200) {
+        // If the registration is successful, you can handle the response here
 
-      
-      }else{ console.log(response.status)}
-   
+        Login(email, password);
+        setFormData({
+          name: "",
+          email: "",
+          password: "",
+          password2: "",
+        });
+
+
+      } else { console.log(response.status) }
+
     } catch (error) {
       // If there's an error, handle it here
       console.error("Registration error", error.response.data);
       setErrors(error.response.data);
       setToastOpen(true);
-      
+
     }
   };
 
@@ -81,18 +81,18 @@ const [errors,setErrors] = useState({});
 
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
-     setToastOpen(false);
+      setToastOpen(false);
       return;
     }
   }
 
 
-  const Login = async (userName,passWord) => {
+  const Login = async (userName, passWord) => {
     const phrase = "somaiya.edu";
 
     if (!userName.endsWith(phrase)) {
 
-      console.log("The string does not end with 'somaiya.edu : ",userName);
+      console.log("The string does not end with 'somaiya.edu : ", userName);
       return;
     }
 
@@ -117,7 +117,7 @@ const [errors,setErrors] = useState({});
       if (result.success) {
         if (result.token) {
           localStorage.setItem("token", result.token);
-       
+
           navigate("/home");
         } else {
           console.log(result.token);
@@ -137,19 +137,32 @@ const [errors,setErrors] = useState({});
   return (
     <div className="login-main-div overflow-hidden">
       <Navbar />
+
       {/* daksh ye div mene add kiya hai image and login ko ek container mein daalne k liye  */}
-      <div className="flex md:space-x-14 lg:space-x-6 bg-[#f3f2f7] h-full justify-center">
+      <div className="flex md:space-x-14 lg:space-x-14 h-full justify-center">
+
         {/* ye img hai login card k baju wala */}
         <div>
-          <img
-            src="https://media.istockphoto.com/id/1452604857/photo/businessman-touching-the-brain-working-of-artificial-intelligence-automation-predictive.webp?b=1&s=170667a&w=0&k=20&c=iJp6e2C-l2lRmyG3ColHMpXe0QYrPnrfQQc2O6PsYC4="
-            alt="help"
-            className="h-[32rem] hidden md:block md:w-[58rem] lg:w-[48rem]  ml-8 mt-10 rounded"
-          />
+          <img src="https://www.alphalearn.com/wp-content/uploads/2021/11/e-learning-2.jpg" alt="help" className="h-[31rem] hidden rounded md:block
+          // responsive design css
+          md:w-[30rem] lg:w-[48rem] ml-8 mt-10 2xl:w-[55rem] 2xl:h-[37rem]" />
         </div>
+
+
         {/* This is login card */}
-        <div className="mt-28 mb-[200px] hover:ring-4 ring-blue-950 ring-offset-4 rounded-[20px] md:w-[33rem] md:ml-0 lg:w-[33rem] lg:ml-0">
-          <form onSubmit={handleSubmit} className="login-card ">
+        <div className="md:mt-[5.5rem] mb-[200px] rounded-[20px] 
+        // responsive css design 
+      md:w-[28rem] md:ml-0 md:mr-24 md:h-96
+      lg:w-[33rem] lg:ml-0">
+
+          <form onSubmit={handleSubmit} className="login-card space-y-10 mt-20 md:mt-0 sm:mr-5 md:space-y-5 
+           2xl:space-y-10">
+
+<div className="input-group text-2xl font-bold">
+            {/* yape font awesome ka icon add krna hai */}
+              SIGNUP
+            </div>
+
             <div className="input-group">
               <input
                 type="text"
@@ -161,6 +174,7 @@ const [errors,setErrors] = useState({});
                 required={true}
               ></input>
             </div>
+
             <div className="input-group">
               <input
                 type="email"
@@ -172,48 +186,50 @@ const [errors,setErrors] = useState({});
                 required={true}
               ></input>
             </div>
+
             <div className="input-group" style={{ display: "flex", alignItems: "center" }}>
-  <input
-    type={passwordShown ? "text" : "password"}
-    id="password"
-    className=" fa-solid fa-lock rounded py-1 text-center bg-gray-200 placeholder-black border-b-2 border-black text-black"
-    name="password"
-    placeholder="Enter Password"
-    value={password}
-    onChange={handleChange}
-    required={true}
-  />
-  <i onClick={togglePasswordVisiblity} className="eyeImgsec" style={{ marginLeft: "10%", cursor: "pointer",color:"black" }}>
-    {eye}
-  </i>
-</div>
-                <div className="input-group" style={{ display: "flex", alignItems: "center" }}>
-  <input
-    type={passwordShown2 ? "text" : "password"}
-    id="password2"
-    className=" fa-solid fa-lock rounded py-1 text-center bg-gray-200 placeholder-black border-b-2 border-black text-black"
-    name="password2"
-    placeholder="Confirm Password"
-    value={password2}
-    onChange={handleChange}
-    required={true}
-  />
-  <i onClick={togglePasswordVisiblity2} className="eyeImgsec" style={{ marginLeft: "10%", cursor: "pointer",color:"black" }}>
-    {eye2}
-  </i>
-</div>
-            <div className="sm:ml-[0rem] md:ml-[0rem] lg:ml-[3.5rem] space-y-1">
-              <div className="flex ">
+              <input
+                type={passwordShown ? "text" : "password"}
+                id="password"
+                className=" fa-solid fa-lock rounded py-1 text-center bg-gray-200 placeholder-black border-b-2 border-black text-black"
+                name="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={handleChange}
+                required={true}
+              />
+              <i onClick={togglePasswordVisiblity} className="eyeImgsec" style={{ marginLeft: "10%", cursor: "pointer", color: "black" }}>
+                {eye}
+              </i>
+            </div>
+
+            <div className="input-group" style={{ display: "flex", alignItems: "center" }}>
+              <input
+                type={passwordShown2 ? "text" : "password"}
+                id="password2"
+                className=" fa-solid fa-lock rounded py-1 text-center bg-gray-200 placeholder-black border-b-2 border-black text-black"
+                name="password2"
+                placeholder="Confirm Password"
+                value={password2}
+                onChange={handleChange}
+                required={true}
+              />
+              <i onClick={togglePasswordVisiblity2} className="eyeImgsec" style={{ marginLeft: "10%", cursor: "pointer", color: "black" }}>
+                {eye2}
+              </i>
+            </div>
+
+            <div className=" space-y-1 self-center ">
+              <div className="flex">
                 <hr className="bg-black text-black w-[7rem] lg:w-[10rem] mt-3 mr-3" />
                 OR
                 <hr className="bg-black text-black w-[7rem] lg:w-[10rem] mt-3 ml-3" />
               </div>
-              <div>
-                Already A User?{" "}
-                <span onClick={() => navigate("/login")}>Login</span>
-              </div>
+
+              <div>Not an Account? <span onClick={() => { navigate("/signup") }}>Signup</span></div>
             </div>
-            <div className="sm:ml-[0rem] md:ml-[4rem] lg:ml-[10rem] bg-[#CCC9DC] border-2 border-blue-950 text-black px-10  font-bold py-0.5 rounded-lg text-xl font-serif hover:ring-2 ring-[#CCC9DC]">
+            {/* sm:ml-[0rem] md:ml-[4rem] lg:ml-[10rem] */}
+            <div className=" bg-[#CCC9DC] border-2 border-blue-950 text-black px-10  font-bold py-0.5 rounded-lg text-xl font-serif hover:ring-2 ring-[#CCC9DC] self-center">
               <button type="submit" className="login-submit-btn">
                 Signup
               </button>
@@ -221,16 +237,16 @@ const [errors,setErrors] = useState({});
           </form>
         </div>
         <Snackbar open={toastOpen} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
-  <Alert onClose={handleClose} severity="error"     sx={{ width: '100%' }}>
-  <ul>
-        {Object.entries(errors).map(([key, value]) => (
-          <li key={key}>
-          {value}
-          </li>
-        ))}
-      </ul>
-  </Alert>
-</Snackbar>
+          <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            <ul>
+              {Object.entries(errors).map(([key, value]) => (
+                <li key={key}>
+                  {value}
+                </li>
+              ))}
+            </ul>
+          </Alert>
+        </Snackbar>
       </div>
     </div>
   );
